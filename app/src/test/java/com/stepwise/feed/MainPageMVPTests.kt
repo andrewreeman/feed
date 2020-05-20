65d4580240f81940e2c741095adbb44e6c4e4860
areeman <andrew.reeman@gmail.com>
2020-05-20 19:08:11 +0100

    With unit tests for new item creation

--- app/src/test/java/com/stepwise/feed/MainPageMVPTests.kt
+++ app/src/test/java/com/stepwise/feed/MainPageMVPTests.kt
@@ -1,11 +1,15 @@
 package com.stepwise.feed
 
+import android.content.res.Resources
 import com.stepwise.feed.mainpage.MainPageMVP
 import com.stepwise.feed.mainpage.MainPageViewModel
 import com.stepwise.feed.mainpage.Presenter
+import com.stepwise.feed.mainpage.addcontent.CreateNewItemErrorViewModel
 import com.stepwise.feed.mainpage.contentlist.ContentListItemViewModel
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
 import org.mockito.Mockito.*
 
 class MainPageMVPTests {
@@ -18,7 +22,7 @@ class MainPageMVPTests {
     fun setup() {
         mockModel = mock(MainPageMVP.Model::class.java)
         mockView = mock(MainPageMVP.View::class.java)
-        presenter = Presenter(mockModel)
+        presenter = Presenter(mockModel, mock(Resources::class.java, RETURNS_MOCKS))
         presenter.setView(mockView)
 
         item = ContentListItemViewModel("A title", "A description")
@@ -41,5 +45,47 @@ class MainPageMVPTests {
     fun navigateToAddItemCalled_WhenOnAddItemTapped() {
         presenter.onAddItemTapped()
         verify(mockView, times(1)).navigateToAddItem()
+    }
+
+    @Test
+    fun creatingNewItem_WithoutTitle_WillShowError() {
+        presenter.createNewItem("", "Description")
+
+        val expectedErrorVM = CreateNewItemErrorViewModel("", null)
+        verify(mockView, times(1)).createNewItemError(expectedErrorVM)
+        verify(mockView, never()).onNewItemCreated(MockitoHelper.anyObject())
+        verify(mockModel, never()).createNewItem(MockitoHelper.anyObject(), MockitoHelper.anyObject())
+    }
+
+    @Test
+    fun creatingNewItem_WithoutDescription_WillShowError() {
+        presenter.createNewItem("Title", "")
+
+        val expectedErrorVM = CreateNewItemErrorViewModel(null, "")
+        verify(mockView, times(1)).createNewItemError(expectedErrorVM)
+        verify(mockView, never()).onNewItemCreated(MockitoHelper.anyObject())
+        verify(mockModel, never()).createNewItem(MockitoHelper.anyObject(), MockitoHelper.anyObject())
+    }
+
+    @Test
+    fun creatingNewItem_Correctly_WillCreateNewItemInModel() {
+        val newItem = ContentListItemViewModel("Title", "Description")
+        `when`(mockModel.createNewItem("Title", "Description")).thenReturn(newItem)
+
+        presenter.createNewItem("Title", "Description")
+
+        verify(mockView, never()).createNewItemError(MockitoHelper.anyObject())
+        verify(mockView, times(1)).onNewItemCreated(newItem)
+        verify(mockModel, times(1)).createNewItem("Title", "Description")
+    }
+
+    // Using Mockito matchers with kotlin: http://derekwilson.net/blog/2018/08/23/mokito-kotlin
+    object MockitoHelper {
+        fun <T> anyObject(): T {
+            any<T>()
+            return uninitialized()
+        }
+        @Suppress("UNCHECKED_CAST")
+        fun <T> uninitialized(): T =  null as T
     }
 }
