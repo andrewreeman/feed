8f6d3348edfeeb4617616da5c4d23b26f8775a68
areeman <andrew.reeman@gmail.com>
2020-05-20 18:21:28 +0100

    With new item validation in MVP interfaces

--- app/src/main/java/com/stepwise/feed/mainpage/MainPageMVP.kt
+++ app/src/main/java/com/stepwise/feed/mainpage/MainPageMVP.kt
@@ -2,21 +2,25 @@
 
 package com.stepwise.feed.mainpage
 
+import com.stepwise.feed.mainpage.addcontent.CreateNewItemErrorViewModel
 import com.stepwise.feed.mainpage.contentlist.ContentListItemViewModel
 
 interface MainPageMVP {
     interface View {
         fun updateContent(viewModel: MainPageViewModel)
         fun navigateToAddItem()
+        fun createNewItemError(error: CreateNewItemErrorViewModel)
     }
 
     interface Presenter {
         fun setView(view: MainPageMVP.View)
         fun loadContent()
         fun onAddItemTapped()
+        fun createNewItem(title: String, description: String)
     }
 
     interface Model {
+        fun createNewItem(title: String, description: String)
         fun getContent(): List<ContentListItemViewModel>
     }
 }
---
+++ app/src/main/java/com/stepwise/feed/mainpage/addcontent/CreateNewItemErrorViewModel.kt
@@ -0,0 +1,3 @@
+package com.stepwise.feed.mainpage.addcontent
+
+data class CreateNewItemErrorViewModel(val titleError: String?, val descriptionError: String?)
